68a35612571db3c33b25a1a8172df6516248f7b8
MaartenQHuijser <maarten@huijser.nl>
2020-01-29 17:24:31 +0100

    update menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,6 +2,8 @@
     //Get hamburger icon and menu
     const hamburger = document.getElementById('js-nav__hamburger');
     const mobileMenu = document.getElementById('js-nav__items');
+    const children = mobileMenu.childNodes;
+    const childrenArray = [...children];
 
     //Add event listener and add/remove classes on click
     hamburger.addEventListener('click', function () {
@@ -13,4 +15,11 @@
             hamburger.classList.add('open');
         }
     })
+
+    childrenArray.forEach(function (item) {
+        item.addEventListener('click', function () {
+            mobileMenu.classList.remove('nav__items--open');
+            hamburger.classList.remove('open');
+        })
+    });
 })();
